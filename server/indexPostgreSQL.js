226f42618852b3e8926b7c154001e1d5cc6c484b
Andrew Ngo <andrew@Andrews-MacBook-Pro-3.local>
2018-10-11 14:48:01 -0700

    Includes proxy endpoint

--- server/indexPostgreSQL.js
+++ server/indexPostgreSQL.js
@@ -28,7 +28,7 @@ const checkOutWidget = 'ec2-52-53-226-39.us-west-1.compute.amazonaws.com'
 
 app.use('/:id', express.static(path.join(__dirname, 'public')));
 
-
+app.get('/checkout/:id', proxy(checkOutWidget));
 
 // app.use(bodyParser.json());
 // // app.use((req, res, next) => {
